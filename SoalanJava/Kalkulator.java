7683760e3dbb1fd48244a76f1969b3a924eca074
zureenanr <108532824+zureenanr@users.noreply.github.com>
2022-12-09 16:44:10 +0800

    Add files via upload

---
+++ SoalanJava/Kalkulator.java
@@ -0,0 +1,23 @@
+public class Kalkulator {
+    public static void main(String[] args) {
+
+        //jenis data
+        double cm, cf, kb;
+
+        System.out.println("nilai unit mm kepada cm");
+        System.out.println("nilai calcius kepada fahrenheit");
+        System.out.println("nilai calcius kepada fahrenheit");
+
+    
+        //Operasi
+         
+
+        //output
+        System.out.println(" 35.5 Milimeter kepada Centimeter ialah: ");
+        System.out.println(" 87.67 Calcius kepada Fahrenheit ialah: ");
+        System.out.println(" 56.99 Kilometer kepada Batu ialah: ");
+
+
+        
+    }
+}
